fcb1164b8f9fdd336a1568eb646a06bb11685cc9
FeiWangHub <fei.wang.cn@icloud.com>
2023-03-24 00:49:41 +0800

    Feature: improve openai completion support

--- src/main/java/com/feiwanghub/openai/service/ChatGPTService.java
+++ src/main/java/com/feiwanghub/openai/service/ChatGPTService.java
@@ -1,6 +1,10 @@
 package com.feiwanghub.openai.service;
 
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.service.OpenAiService;
 import okhttp3.*;
+import org.jetbrains.annotations.TestOnly;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +22,30 @@ public class ChatGPTService {
 
     public String generateText(String prompt) throws IOException {
         RequestBody requestBody = RequestBody.create(
-                "{\"prompt\": \"" + prompt + "\", \"temperature\": 0.5}",
+                "{\"prompt\": \"" + prompt + "\", \"temperature\": 0.5, \"model\": \"text-davinci-003\", \"max_tokens\": 100}",
                 JSON
         );
         Request request = new Request.Builder()
-                .url("https://api.openai.com/v1/engines/davinci-codex/completions")
+                .url("https://api.openai.com/v1/completions")
                 .addHeader("Authorization", "Bearer " + apiKey)
                 .post(requestBody)
                 .build();
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
         }
+    }
+
+    @TestOnly
+    public String generateTextByOpenAI(String prompt) {
+        OpenAiService service = new OpenAiService(apiKey);
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt(prompt)
+                .maxTokens(100)
+                .temperature(0.5)
+                .build();
+        CompletionResult completion = service.createCompletion(completionRequest);
+        completion.getChoices().forEach(choice -> System.out.println(choice.getText()));
+        return completion.getChoices().get(0).getText();
     }
 }
 
