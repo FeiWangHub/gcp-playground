7d167c536ed15b90b501f7f03c9536bd92390f8b
FeiWangHub <fei.wang.cn@icloud.com>
2023-08-26 12:59:00 +0800

    Dataflow: improve

--- sub-dataflow-new/src/main/java/com/feiwanghub/subdataflownew/wordcount/WordCount.java
+++ sub-dataflow-new/src/main/java/com/feiwanghub/subdataflownew/wordcount/WordCount.java
@@ -4,15 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Flatten;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static com.feiwanghub.subdataflownew.wordcount.ExtractWordListFn.TOKENIZER_PATTERN;
+
+/**
+ * Official Word Count Code
+ * <a href="https://github.com/apache/beam/blob/master/examples/java/src/main/java/org/apache/beam/examples/WordCount.java">Official Word Count Code</a>
+ */
 @Slf4j
 public class WordCount {
 
@@ -31,22 +36,32 @@ public class WordCount {
 
         /**
          * Convert lines of text into list of individual words
-         * the print ParDo prints out world list of each line?? TODO
+         * the print ParDo prints out world list of each line
          */
-        PCollection<List<String>> wordLists = lines.apply(MapElements.via(new ExtractWordListFn()));
-        wordLists.apply(ParDo.of(new PrintListDoFn()));
+        PCollection<List<String>> wordLists = lines
+                .apply(MapElements.via(new ExtractWordListFn()))
+                .apply(Filter.by((List<String> wordList) -> wordList != null && !wordList.isEmpty()));
+        //wordLists.apply(ParDo.of(new PrintListDoFn()));
+
+        // words extract option 2
+        //PCollection<String> words = lines.apply(FlatMapElements.into(TypeDescriptors.strings())
+        //        .via((String line) -> Arrays.asList(line.split(TOKENIZER_PATTERN))));
+        //words.apply(ParDo.of(new PrintStringFn()));
 
         /**
          * Count the number of times each word occurs
          */
-        PCollection<KV<String, Long>> wordCountKV = wordLists.apply(Flatten.iterables()).apply(Count.perElement());
-        PCollection<String> resultStrPool = wordCountKV.apply(ParDo.of(new FormatKVAsTextFn()));
-//        resultStrPool.apply(ParDo.of(new PrintStringFn()));
+        PCollection<KV<String, Long>> wordCountKV = wordLists
+                .apply(Flatten.iterables())
+                .apply(Count.perElement());
+        PCollection<String> resultPool = wordCountKV.apply(ParDo.of(new FormatKVAsTextFn()));
+        resultPool.apply(ParDo.of(new PrintStringFn()));
 
         /**
          * Output to gcp bucket TODO
          */
-        //resultStrPool.apply(TextIO.write().to(options.getOutputFile()));
+        //resultPool.apply(TextIO.write().to(options.getOutputFile()));
+        //resultPool.apply(TextIO.write().to("wordcout.txt"));
 
         pipeline.run().waitUntilFinish();
     }
